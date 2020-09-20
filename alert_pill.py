c516c659b59ea0efabe90068c12d580d1bc149d2
dexelop <dexelop@gmail.com>
2020-09-20 18:24:27 +0900

    2nd commit

--- alert_pill.py
+++ alert_pill.py
@@ -1,8 +1,17 @@
 import os
 import requests
+from datetime import datetime
+
+thistime = datetime.now()
+text = thistime.strftime("%y/%m/%d - %H:%M")
+text = text + " 에 약을 드시길 권해드렸습니다."
 
 url = os.environ.get('SLACK_URL')
+url = "https://hooks.slack.com/services/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"
 
 def slack_post_text(url, text):
     result = requests.post(url, json = {"text": text})
     return result
+
+if __name__ == '__main__':
+    slack_post_text(url, text)
